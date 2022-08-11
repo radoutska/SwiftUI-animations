b2664a17c184db9df7612aae05234738b6221246
Anna Radoutska <aradoutska@griddynamics.com>
2022-08-10 21:15:36 -0400

    small fix

--- SwiftUI-animations/ContentView.swift
+++ SwiftUI-animations/ContentView.swift
@@ -25,40 +25,16 @@ struct ContentView: View {
     
     var body: some View {
         VStack() {
-            Rectangle()
-                .frame(width: 60, height:8, alignment: .center)
-                .cornerRadius(5)
-                .padding(.bottom, 11)
-                .rotationEffect(.degrees(isRotating ? 25 : 0), anchor: .leading)
-
-            Rectangle()
-                .frame(width: 60, height:8, alignment: .center)
-                .cornerRadius(5)
-                .rotationEffect(.degrees(isRotating ? -25 : 0), anchor: .leading)
-        }
-        .onTapGesture {
-            withAnimation (.interpolatingSpring(stiffness: 300, damping: 15)) {
-                isRotating.toggle()
-                isHidden.toggle()
-            }
-        }
-    }
-    
-    let gradientStart = Color(red: 239.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
-    let gradientEnd = Color(red: 239.0 / 255, green: 172.0 / 255, blue: 120.0 / 255)
-    
-    
-    struct Bezier: Shape {
-        func path(in rect: CGRect) -> Path
-        {
-            var path = Path()
-            path.move(to: CGPoint(x: 100, y: 100))
-            //path.addQuadCurve(to: CGPoint, control: CGPoint)
-            return path
+            
         }
         
     }
+
+
+let gradientStart = Color(red: 239.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
+let gradientEnd = Color(red: 239.0 / 255, green: 172.0 / 255, blue: 120.0 / 255)
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
